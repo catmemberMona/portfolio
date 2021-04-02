73a1293f0107228be213794c70f1e823d67761da
Mona Zheng <mona.a.zheng@gmail.com>
2021-04-01 22:14:40 -0400

    add basic components for about Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Mona Zheng</h1>
-      <p>NOTHING IS SUPPOSE TO BE HERE!</p>
+    <div className={classes.root}>
+      {/* About Mona Section */}
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <h1>Mona Zheng</h1>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <p>Image is here</p>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <p>NOTHING IS SUPPOSE TO BE HERE!</p>
+        </Grid>
+      </Grid>
     </div>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 export default App;
