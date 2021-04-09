e136ad566756bdabcb2aec6436b64291d0a22893
Mona Zheng <mona.a.zheng@gmail.com>
2021-04-09 18:17:09 -0400

    add images and video for projects adjusted seed data to remove like, etc and added status and tech sections

--- src/Components/projectData.js
+++ src/Components/projectData.js
@@ -1,39 +1,46 @@
+import mona from '../Images/mona.jpeg'
+
 export default [
   {
-    title: 'This is the Title',
-    content: 'This is the content of the application',
-    likes: 5,
-    comments: 10,
-    views: 14,
+    title: "Mona's Portfolio",
+    status: 'Ongoing - Github: ',
+    content: 'Better looking presentation of projects compared to the resume.',
+    tech: 'Create-React-App, Material-UI, Github pages',
+    image: mona,
+  },
+  {
+    title: 'Memory Maddening',
+    status: 'Complete - Github, Deployed - Heroku',
+    content:
+      'Test your memory and typing skill. Web broswer game created during the Mintbean weekly learnathon challenge. Completed with teammates Jamie and Scott. Not supported for mobile browser.',
+    tech: 'Create-React-App, etc',
     image:
       'http://www.girardatlarge.com/wp-content/uploads/2013/05/gravatar-60-grey.jpg',
   },
   {
-    title: 'This is the 2nd Title',
-    content: 'This is the 2nd content of the application',
-    likes: 5,
-    comments: 10,
-    views: 14,
-    image:
-      'http://www.girardatlarge.com/wp-content/uploads/2013/05/gravatar-60-grey.jpg',
-  },
-  {
-    title: 'This is the 3rd Title',
+    title: 'Simon',
+    status: 'Complete - Github: ',
     content: 'This is the 3rd content of the application',
-    likes: 5,
-    comments: 10,
-    views: 14,
+    tech: '',
     image:
       'http://www.girardatlarge.com/wp-content/uploads/2013/05/gravatar-60-grey.jpg',
   },
   {
-    title: 'This is the 4th Title',
+    title: 'scavengARescape',
+    status: 'Complete - Github: ',
     content:
       'This is the 4th content of the application, this is a long content that needs to be show partially',
-    likes: 5,
-    comments: 10,
-    views: 14,
-    image:
-      'http://www.girardatlarge.com/wp-content/uploads/2013/05/gravatar-60-grey.jpg',
+    tech: '',
+    image: '',
+    video: (
+      <iframe
+        width='560'
+        height='315'
+        src='https://www.youtube-nocookie.com/embed/VGoNcz_78Ns'
+        title='YouTube video player'
+        frameborder='1'
+        allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+      ></iframe>
+    ),
   },
 ];
