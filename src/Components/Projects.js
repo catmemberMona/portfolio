2561c3dad8a5e1a15ad5ceec683b3cd891cedc0e
Mona Zheng <mona.a.zheng@gmail.com>
2021-04-09 15:58:06 -0400

    added subtitle for projects section centered projects section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,25 +2,53 @@ import React from 'react';
 import {
   Divider,
   Container,
+  Typography,
 } from '@material-ui/core';
 import topics from './projectData'
 import Project from './Project';
 
 function Projects() {
   return (
-    <Container maxWidth={false}>
-      <div style={{ maxWidth: 1800, margin: 'auto' }}>
-        {topics.map((topic) => {
-          return (
-            <div>
-              <Project {...topic} />
-              <Divider inset={true} />
-            </div>
-          );
-        })}
-      </div>
-    </Container>
+    <div style={{ maxWidth: 1800, margin: 'auto' }}>
+      <Container maxWidth={false}>
+        <div>
+          <Typography style={styles.subHeading}>About Mona</Typography>
+        </div>
+        <div>
+          {topics.map((topic) => {
+            return (
+              <div>
+                <Project {...topic} />
+                <Divider inset={true} />
+              </div>
+            );
+          })}
+        </div>
+      </Container>
+    </div>
   );
+}
+
+const styles = {
+  externalLinks: {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  avatarLink: {
+    flex: 1,
+    marginTop: '1em',
+  },
+  subHeading: {
+    marginTop: '3em',
+    fontWeight: 600,
+    fontSize: '3em',
+  },
+  contentText: {
+    fontSize: '1.4rem',
+    lineHeight: 1.4,
+  },
 }
 
 export default Projects;
