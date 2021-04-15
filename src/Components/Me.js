f793c6e482e3ecd9e82c93dac07037ef8e1ec696
Mona Zheng <mona.a.zheng@gmail.com>
2021-04-15 17:58:29 -0400

    added main image

--- src/Components/Me.js
+++ src/Components/Me.js
@@ -5,14 +5,18 @@ import {
   Typography,
   Container,
 } from '@material-ui/core';
+import water from '../Images/water.jpeg';
 
 function Me(props) {
   const classes = useStyles();
-  const mona = props.avatarImage;
 
   return (
     <div id='me' style={{ maxWidth: 1200, margin: 'auto' }}>
-      <div style={{ marginBottom: '5em', visibility: 'hidden', marginTop: '-5em' }}>Just spacing</div>
+      <div
+        style={{ marginBottom: '5em', visibility: 'hidden', marginTop: '-5em' }}
+      >
+        Just spacing
+      </div>
       <Container maxWidth={false}>
         <Typography align='center' className={classes.heading}>
           Mona Zheng
@@ -21,7 +25,7 @@ function Me(props) {
       <Container maxWidth={false}>
         <Avatar
           alt='Mona'
-          src={mona}
+          src={water}
           variant='square'
           className={classes.extraLarge}
         />
@@ -34,7 +38,7 @@ const useStyles = makeStyles((theme) => ({
   extraLarge: {
     marginTop: theme.spacing(2),
     width: '100%',
-    height: 510,
+    height: 560,
     minHeight: 510,
   },
   heading: {
