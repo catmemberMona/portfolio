e2751099cf88346e96ebcb22d2af3390795f35c0
Mona Zheng <mona.a.zheng@gmail.com>
2021-04-15 18:25:33 -0400

    updated portfolio project image adjusted img size for memory game project adjusted width of scavengar img size

--- src/Components/projectData.js
+++ src/Components/projectData.js
@@ -1,4 +1,4 @@
-import mona from '../Images/mona.jpeg'
+import portfolioImg from '../Images/portfolioImg.png'
 import memory from '../Images/memory.png'
 import simon from '../Images/simon2.png'
 
@@ -10,7 +10,7 @@ export default [
     content:
       'I created this portfolio page in order to present my goals and projects in more web friendly manner than compared to viewing the bullet points on my resume.',
     tech: 'Create-React-App, Material-UI, Github pages',
-    image: mona,
+    image: portfolioImg,
   },
   {
     title: 'Memory Maddening',
@@ -43,7 +43,7 @@ export default [
     video: (
       <iframe
         display='block'
-        width='90%'
+        width='79%'
         height='100%'
         src='https://www.youtube-nocookie.com/embed/VGoNcz_78Ns'
         title='YouTube video player'
