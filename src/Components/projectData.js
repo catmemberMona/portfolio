5fe59b7136423987abd65cd66dbe262d4bb19e45
Mona Zheng <mona.a.zheng@gmail.com>
2021-05-04 10:10:10 -0400

    added hourly bell tower project information to projects changed memory game name

--- src/Components/projectData.js
+++ src/Components/projectData.js
@@ -1,6 +1,7 @@
 import portfolioImg from '../Images/portfolioImg.png'
 import memory from '../Images/memory.png'
 import simon from '../Images/simon2.png'
+import bellTower from '../Images/bellTowerOn.png'
 
 export default [
   {
@@ -11,6 +12,15 @@ export default [
       "Tell me this doesn't look more visually pleasing compared to looking at my resume.",
     tech: 'Create-React-App, Material-UI, Github pages',
     image: portfolioImg,
+  },
+  {
+    title: 'Hourly Bell Tower',
+    status: 'Ongoing',
+    links: ['https://github.com/catmemberMona/clockTowerBellReminderIOS'],
+    content:
+      'Time always flows forward and the speed of time does not change for individuals. Hourly Bell Tower reminds users that time is always moving forward.A bell will ring every hour every day, starting the user’s day with a set first bell and ending the day with the last bell. This application is for those that want an hourly reminder of the time without thinking about repeatedly setting and shutting off an alarm. A bell will sound for each notification, reminding the user an hour has past. Users can turn the notifications and sound off by tapping the screen and turn them back on by tapping the screen when needed. The user interface will change depending on if it is set to on or off. ',
+    tech: 'Swift, Uikit',
+    image: bellTower,
   },
   {
     title: 'Memory Maddening',
@@ -26,11 +36,11 @@ export default [
     image: memory,
   },
   {
-    title: 'Simon',
+    title: 'Diamond Memory Game',
     status: 'Ongoing',
     links: ['https://github.com/catmemberMona/simon-says'],
     content:
-      'I was reminded of this good childhood game when I was playing the current popular game AmongUs. So I created a mobile version of it. It is currently not deployed, but will contain updates and be packaged to the Apple App Store in the future.',
+      'I was reminded of this good childhood game when I was playing the current popular game AmongUs. So I created a mobile version of it. It is currently not deployed, but will contain updates and be packaged to apple TestFlight in the future.',
     tech: 'React-Native, React, Async-Storage, React-Native-Sound',
     image: simon,
   },
