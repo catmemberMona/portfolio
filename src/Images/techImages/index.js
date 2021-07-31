dd995d2bf702b5b54d7006ae23891ea7c22851c9
Mona Zheng <mona.a.zheng@gmail.com>
2021-07-31 11:05:20 -0400

    display the tech images

---
+++ src/Images/techImages/index.js
@@ -0,0 +1,22 @@
+import apple from './apple.png'
+import cocoapods from './cocoapods.svg'
+import css from './css.svg'
+import expressjs from './expressjs.svg'
+import firebase from './firebase.svg'
+import git from './git.svg'
+import github from './github.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import nodejs from './nodejs.svg'
+import postgresql from './postgresql.svg'
+import reactImg from './react.svg'
+import reduxImg from './redux.svg'
+import sequelizeImg from './sequelize.svg'
+import swiftImg from './swift.svg'
+import vscodeImg from './vscode.png'
+import xcodeImg from './xcode.png'
+
+export {
+  apple, cocoapods, css, expressjs, firebase, git, github, html, javascript, nodejs, postgresql, reactImg, reduxImg, sequelizeImg
+  , swiftImg, vscodeImg, xcodeImg
+}
